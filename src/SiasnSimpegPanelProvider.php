da6caca2ce4484f6e9e87065f8590f95b5c5d73f
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2024-03-09 17:46:01 +0700

    feature: add ->spa() ->unsavedChangesAlerts()  ->databaseTransactions() feature

--- src/SiasnSimpegPanelProvider.php
+++ src/SiasnSimpegPanelProvider.php
@@ -15,7 +15,6 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
-use ShuvroRoy\FilamentSpatieLaravelBackup\FilamentSpatieLaravelBackupPlugin;
 
 class SiasnSimpegPanelProvider extends PanelProvider
 {
@@ -48,7 +47,12 @@ class SiasnSimpegPanelProvider extends PanelProvider
                 //
             ])
             ->navigationGroups(config('siasn-simpeg.filament.navigationGroups'))
-            ->plugin(FilamentSpatieLaravelBackupPlugin::make())
+            ->plugins([
+                \ShuvroRoy\FilamentSpatieLaravelBackup\FilamentSpatieLaravelBackupPlugin::make(),
+            ])
+            ->spa()
+            ->unsavedChangesAlerts()
+            ->databaseTransactions()
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
